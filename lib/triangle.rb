e62342d6af76ded70a162aa83e444f2d0dd14a72
Matt <mbrowne108@gmail.com>
2022-01-12 13:53:11 -0500

    commit tree 16458d083a580736bd49058ea182d39c90f9b2f3

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_reader :length1, :length2, :length3
+
+  def initialize (length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    raise TriangleError unless [length1, length2, length3].all?{|x| x > 0} && triangle_equality_valid
+    if length1 == length2 && length2 == length3
+      :equilateral
+    elsif length1 == length2 || length2 == length3 || length1 == length3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def triangle_equality_valid
+    length1 + length2 > length3 && length2 + length3 > length1 && length1 + length3 > length2
+  end
+
+  class TriangleError < StandardError
+    def message
+      "this isn't a valid triangle"
+    end
+  end
 end
